8d3b4da2d6b42548a3e6cf8d88810e74fb4e0726
George Psaras <zpsaras@mit.edu>
2016-01-12 10:20:17 -0500

    Fixed engine/player/sconstruct issues

---
+++ bots/bestbot/src/pokerbots/player/Board.java
@@ -0,0 +1,41 @@
+package pokerbots.player;
+
+import java.util.ArrayList;
+
+public class Board {
+	public ArrayList<ArrayList<Card>> board = new ArrayList<ArrayList<Card>>();
+	public ArrayList<Card> hearts = new ArrayList<Card>();
+	public ArrayList<Card> spades = new ArrayList<Card>();
+	public ArrayList<Card> clubs = new ArrayList<Card>();
+	public ArrayList<Card> diamonds = new ArrayList<Card>();
+
+	public Board(){
+		//Put empty array for each possible value of a card
+		for(int i=0; i<15; i++){
+			ArrayList<Card> temp = new ArrayList<Card>();
+			board.add(temp);
+		}
+	}
+	
+	public void addCard(Card new_card){
+		//Organize cards by numbers
+		board.get(new_card.value).add(new_card);
+		if(new_card.value==14){
+			board.get(1).add(new_card);
+		}
+		//Organize cards by suits
+		if(new_card.suit=="h"){
+			hearts.add(new_card);
+		}
+		else if(new_card.suit=="s"){
+			spades.add(new_card);
+		}
+		else if(new_card.suit=="c"){
+			clubs.add(new_card);
+		}
+		else if(new_card.suit=="d"){
+			diamonds.add(new_card);
+		}
+	}
+	
+}
